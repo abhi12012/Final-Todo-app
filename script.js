f7debaab5469ccdcb2d21891255e32c6e9d6ba43
abhi12012 <agrawalabhishek211@gmail.com>
2026-06-06 13:31:01 +0530

    added delete task feature

--- script.js
+++ script.js
@@ -10,11 +10,24 @@ function addTask() {
 
 
     const li = document.createElement("li");
-    li.textContent = taskText;
+    li.textContent = taskText + " ";
 
+    const span = document.createElement("span");
+    span.textContent = "❌"; 
+    span.style.cursor = "pointer";
+    span.style.color = "red";
+    span.style.float = "right"; 
+
+    li.appendChild(span);
 
     document.getElementById("taskList").appendChild(li);
 
 
-    input.value = ""; // Clear input after adding
+    input.value = ""; 
+
+    span.addEventListener("click", function () {
+        li.remove(); 
+    });
 }
+
+
